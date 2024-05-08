b81eee21a7cda9079968c63943faf5326a0711ba
Oskar Dąbrowski <oskar.ariel.dabrowski@gmail.com>
2024-05-08 18:47:56 +0200

    Make a Database Migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,35 +1,37 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
-# Initialize Flask extensions outside of the create_app function
+# Tworzenie instancji rozszerzeń
 db = SQLAlchemy()
 login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
-
-    # Application configuration
+    
+    # Konfiguracja aplikacji
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///yourdatabase.db'
     app.config['SECRET_KEY'] = 'your-secret-key'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    # Initialize extensions with the application
+    
+    # Inicjalizacja rozszerzeń z aplikacją
     db.init_app(app)
+    migrate = Migrate(app, db)  # Inicjalizacja migracji
     login_manager.init_app(app)
     login_manager.login_view = 'auth.login'
-
-    # Import and registration of blueprints
+    
+    # Rejestracja blueprintów
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
-
+    
     from .routes import main as main_blueprint
     app.register_blueprint(main_blueprint)
-
-    # User loader for Flask-Login
+    
+    # Funkcja ładowania użytkownika dla Flask-Login
     @login_manager.user_loader
     def load_user(user_id):
         from .models import User
         return User.query.get(int(user_id))
-
+    
     return app
