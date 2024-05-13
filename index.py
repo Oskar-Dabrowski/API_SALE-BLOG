b55a5363e585634c7f6a081ac4829887c2bdf03f
Oskar Dąbrowski <oskar.ariel.dabrowski@gmail.com>
2024-05-13 20:00:16 +0200

    vercel bugfix 404

--- index.py
+++ index.py
@@ -1,13 +1,15 @@
 from flask import Flask, jsonify
+from asgiref.wsgi import WsgiToAsgi
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def home():
     return "Flask Vercel API - Working!", 200
 
-
 @app.errorhandler(404)
 def page_not_found(e):
     return jsonify({"status": 404, "message": "Not Found"}), 404
+
+# Convert WSGI app to ASGI app
+asgi_app = WsgiToAsgi(app)
